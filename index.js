588457ba402de7b40afe423233b7f3eca4f6f99a
Todd Wolfson <todd@twolfson.com>
2012-04-03 00:18:36 -0700

    Mozilla samples check out

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 (function () {
-console.log('Hello World!');
+// TODO: Feature detection
+var audioOutput = new Audio();
+audioOutput.mozSetup(2, 44100);
+
+var samples = new Float32Array(22050);  
+for (var i = 0; i < samples.length ; i++) {  
+ samples[i] = Math.sin( i / 20 );  
+} 
+audioOutput.mozWriteAudio(samples); 
 }());
